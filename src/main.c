93e27cd29e1b30edc314fe89e842b6959cad339a
Haden Collins <collinshaden@gmail.com>
2026-02-05 23:14:18 -0600

    Board now connects to wifi

--- src/main.c
+++ src/main.c
@@ -1,4 +1,115 @@
-#include "secret.h"
-// WIFI_SSID and WIFI_PASSWORD are defined in secret.h
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_event.h"
+#include "esp_wifi.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "secret.h" // Credentials WIFI_SSID and WIFI_PASSWORD are defined here
 
-void app_main() {}
+static EventGroupHandle_t wifi_event_group;
+static int retry_num = 0;
+
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT      BIT1
+#define MAX_RETRY_NUM 5
+
+static void wifi_event_handler(void *arg, esp_event_base_t event_base,
+                          int32_t event_id, void *event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START)
+    {
+        esp_wifi_connect();
+    }
+    else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
+    {
+        if (retry_num < MAX_RETRY_NUM)
+        {
+            esp_wifi_connect();
+            retry_num++;
+            ESP_LOGI("wifi", "Failed to connect to the AP, retrying... (%d/%d)", retry_num, MAX_RETRY_NUM);
+        } else {
+            xEventGroupSetBits(wifi_event_group, WIFI_FAIL_BIT);
+        }
+        ESP_LOGI("wifi", "Disconnected from the AP");
+    }
+    else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP)
+    {
+        xEventGroupSetBits(wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
+
+void wifi_init_sta()
+{
+    wifi_event_group = xEventGroupCreate();
+    ESP_ERROR_CHECK_WITHOUT_ABORT(esp_event_loop_create_default());
+    // TODO: implement error handling for everything
+    // Create Network Interface
+    ESP_ERROR_CHECK_WITHOUT_ABORT(esp_netif_init());
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK_WITHOUT_ABORT(esp_wifi_init(&cfg));
+
+    // Setup event loop and handlers
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    ESP_ERROR_CHECK_WITHOUT_ABORT(esp_event_handler_instance_register(WIFI_EVENT,
+                                                                ESP_EVENT_ANY_ID,
+                                                                &wifi_event_handler,
+                                                                NULL,
+                                                                &instance_any_id));
+    ESP_ERROR_CHECK_WITHOUT_ABORT(esp_event_handler_instance_register(IP_EVENT,
+                                                                IP_EVENT_STA_GOT_IP,
+                                                                &wifi_event_handler,
+                                                                NULL,
+                                                                &instance_got_ip));
+
+    // Configure Wi-Fi connection and start the interface
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = WIFI_SSID,
+            .password = WIFI_PASSWORD
+        }
+    };
+    ESP_ERROR_CHECK_WITHOUT_ABORT(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK_WITHOUT_ABORT(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+
+    // Start Wi-Fi
+    ESP_ERROR_CHECK_WITHOUT_ABORT(esp_wifi_start());
+    ESP_LOGI("wifi", "Wi-Fi initialization completed.");
+
+    // Wait for connection or failure
+    EventBits_t bits = xEventGroupWaitBits(wifi_event_group,
+            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+            pdFALSE,
+            pdFALSE,
+            portMAX_DELAY);
+
+    if (bits & WIFI_CONNECTED_BIT)
+    {
+        ESP_LOGI("wifi", "Connected to AP");
+    }
+    // TODO: Need to handle failure eventually and retry after 10 minutes
+    else if (bits & WIFI_FAIL_BIT)
+    {
+        ESP_LOGI("wifi", "Failed to connect to AP");
+    } else {
+        ESP_LOGE("wifi", "UNEXPECTED EVENT");
+    }
+
+}
+
+void app_main()
+{
+    // Wi-Fi requires NVS flash to store credentials otherwise it will fail to initialize
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK_WITHOUT_ABORT(ret);
+
+    wifi_init_sta();
+}
