26079ac9ab8c8e2ef7c88ab92fa87c38b617c754
Haden Collins <collinshaden@gmail.com>
2026-02-14 17:29:37 -0600

    Add additional fonts for UI

--- include/fonts.h
+++ include/fonts.h
@@ -9,5 +9,28 @@ typedef struct {
 } font_t;
 
 extern font_t font16;
+extern font_t font20;
+extern font_t font24;
+extern font_t font40;
+extern font_t font60;
+
+/* Font60 only has 0-9, :, A, P, M for time display to save space
+ * Thus isn't ASCII encoded, and the offsets need to be defined manually */
+enum {
+    ONE = 0,
+    TWO = 510,
+    THREE = 1020,
+    FOUR = 1530,
+    FIVE = 2040,
+    SIX = 2550,
+    SEVEN = 3060,
+    EIGHT = 3570,
+    NINE = 4080,
+    ZERO = 4590,
+    COLON = 5100,
+    LETTER_A = 5610,
+    LETTER_P = 6120,
+    LETTER_M = 6630,
+};
 
 #endif
