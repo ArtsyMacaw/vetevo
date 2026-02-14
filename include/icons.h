74c87cfebbe899d8ef5c2acd2905fdd590562730
Haden Collins <collinshaden@gmail.com>
2026-02-14 17:27:24 -0600

    Add basic icons for the UI

---
+++ include/icons.h
@@ -0,0 +1,13 @@
+#ifndef ICONS_H
+#define ICONS_H
+extern const unsigned char icon_cloud[1545];
+extern const unsigned char icon_rain[1545];
+extern const unsigned char icon_drizzle[1545];
+extern const unsigned char icon_thunder[1545];
+extern const unsigned char icon_haze[1545];
+extern const unsigned char icon_snow[1545];
+extern const unsigned char icon_sun[1545];
+extern const unsigned char icon_moon[1545];
+extern const unsigned char icon_wind[240];
+extern const unsigned char icon_cloud_small[240];
+#endif
